501e0bff9502c4ba7e70184cefc65476c1f46668
Jonathan Perkins <jonathanperkins@Jonathans-MacBook-Pro.local>
2017-03-19 21:51:36 -0500

    first commit

---
+++ models/author.js
@@ -0,0 +1,40 @@
+var mongoose = require('mongoose');
+var moment = require('moment');
+
+var Schema = mongoose.Schema;
+
+var AuthorSchema = Schema({
+	first_name: {type: String, required: true, max: 100},
+	family_name: {type: String, required: true, max: 100},
+	date_of_birth: {type: Date},
+	date_of_death: {type: Date}
+})
+
+//Virtual for author's full name
+AuthorSchema
+	.virtual('name')
+	.get(function () {
+		return this.family_name + ', ' + this.first_name;
+});
+
+//Virtual for author's full name
+AuthorSchema
+	.virtual('url')
+	.get(function () {
+		return '/catalog/author/' + this._id;
+});
+
+AuthorSchema
+	.virtual('dob')
+	.get(function () {
+		return this.date_of_birth ? moment(this.date_of_birth).format('YYYY-MM-DD') : '';
+});
+
+AuthorSchema
+	.virtual('dod')
+	.get(function () {
+		return this.date_of_death ? ' to ' + moment(this.date_of_death).format('YYYY-MM-DD') : '';
+});
+
+//Export model
+module.exports = mongoose.model('Author', AuthorSchema);
